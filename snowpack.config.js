cd4d36752d1a7de6818a8f8e6e55032d11ce7a89
Cal Smith <1744185+cal-smith@users.noreply.github.com>
2021-05-28 22:41:44 +0000

    fix prod worker path

--- snowpack.config.js
+++ snowpack.config.js
@@ -8,7 +8,7 @@ module.exports = {
   },
   env: {
     DEV_WORKER: 'http://127.0.0.1:8787',
-    PROD_WORKER: 'https://covid-data.calsmith.workers.dev/'
+    PROD_WORKER: 'https://covid-data.calsmith.workers.dev'
   },
   plugins: [
     [
